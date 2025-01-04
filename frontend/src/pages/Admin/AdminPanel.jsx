d42b5f6fd3441789850f8f500ef305fe236b6cb9
Abhay Hitendra Dusane <abhayhd_Chrome@skncoe365.onmicrosoft.com>
2025-01-04 23:37:07 +0530

    Adjust padding in AdminPanel for improved layout

--- frontend/src/pages/Admin/AdminPanel.jsx
+++ frontend/src/pages/Admin/AdminPanel.jsx
@@ -15,7 +15,7 @@ const AdminPanel = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-2xl mx-auto p-4">
+      <div className="max-w-2xl mx-auto p-4 mt-16">
         <Tab.Group>
           <Tab.List className="flex space-x-4 border-b mb-6">
             <Tab
